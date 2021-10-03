b79e0604b8e4d8b3c9ef44b6728ca177580a7655
Jovan Slavujevic <jotzilla96@gmail.com>
2021-10-03 14:42:58 +0200

    - Final implementation of SW module - Added PCB Design of board

--- ArduinoSW/UC_7Seg_Switch/UC_7Seg_Switch.ino
+++ ArduinoSW/UC_7Seg_Switch/UC_7Seg_Switch.ino
@@ -1,54 +1,72 @@
 /* @brief ESSENTIAL MACROS */
 #define ARD_PIN_SEG_A 0x00u
+#define ARD_PIN_SEG_B 0x01u
+#define ARD_PIN_SEG_C 0x02u
+#define ARD_PIN_SEG_D 0x03u
+#define ARD_PIN_SEG_E 0x04u
+#define ARD_PIN_SEG_F 0x05u
 #define ARD_PIN_SEG_G 0x06u
 
 #define ARD_PIN_SW_1  0x07u
-#define ARD_PIN_SW_3  0x08u
+#define ARD_PIN_SW_2  0x08u
+#define ARD_PIN_SW_3  0x09u
+
+#define ARD_PIN_LED_1 0x0Bu
+#define ARD_PIN_LED_2 0x0Cu
+#define ARD_PIN_LED_3 0x0Du
 
 #define SEGMENTS      0x07u
-#define NUM_OF_SW     0x02u
+#define NUM_OF_SW     0x03u
 
 /* @brief representation of 7-seg enumerations in array of booleans */
 ///////////////////////////{a,b,c,d,e,f,g}
-static const bool seg0[] = {0,0,0,0,0,0,1};
-static const bool seg1[] = {1,0,0,1,1,1,1};
-static const bool seg2[] = {0,0,1,0,0,1,0};
-static const bool seg3[] = {0,0,0,0,1,1,0};
-static const bool seg4[] = {1,0,0,1,1,0,0};
-static const bool seg5[] = {0,1,0,0,1,0,0};
-static const bool seg6[] = {0,1,0,0,0,0,0};
-static const bool seg7[] = {0,0,0,1,1,1,1};
-static const bool seg8[] = {0,0,0,0,0,0,0};
-static const bool seg9[] = {0,0,0,0,1,0,0};
+static const bool seg0[] = {1,1,1,1,1,1,0};
+static const bool seg1[] = {0,1,1,0,0,0,0};
+static const bool seg2[] = {1,1,0,1,1,0,1};
+static const bool seg3[] = {1,1,1,1,0,0,1};
+static const bool seg4[] = {0,1,1,0,0,1,1};
+static const bool seg5[] = {1,0,1,1,0,1,1};
+static const bool seg6[] = {1,0,1,1,1,1,1};
+static const bool seg7[] = {1,1,1,0,0,0,0};
+static const bool seg8[] = {1,1,1,1,1,1,1};
+static const bool seg9[] = {1,1,1,1,0,1,1};
 
 /* @brief array of pointers that are pointing into array of booleans in order to iterate through them */
 static const bool (* const segPtr[])[SEGMENTS] = {&seg0,&seg1,&seg2,&seg3,&seg4,&seg5,&seg6,&seg7,&seg8,&seg9};
-
 static const size_t segPtrSize = (sizeof(segPtr) / sizeof(bool*));
+
+static const bool led_up[]     = {0,0,1};
+static const bool led_down[]   = {0,1,0};
+static const bool led_random[] = {1,0,0};
+static const bool led_none[]   = {0,0,0};
+
+enum e_actions 
+{
+  UP = 0,
+  DOWN,
+  RAND,
+  NONE
+};
 
 /* @brief class handles SW1 & SW3 init, reading and informs system of state changing */
 class CSwitchHanlder
 {
 private:  
-  int SW1_last_state;
-  int SW3_last_state;
-
-  int SW1_current_state;
-  int SW3_current_state;
-
-  bool SW1_enabled;
-  bool SW3_enabled;
+  int SW_last_state[NUM_OF_SW];
+  int SW_current_state[NUM_OF_SW];
+  bool SW_enabled[NUM_OF_SW];
 
   void initSw();
+  void initLed();
   void readSw();
 public:
   CSwitchHanlder();
   ~CSwitchHanlder() = default;
 
+  void updateLed(e_actions action) const;
   void checkSw();
   bool isSwStateUpdated() const;
-  inline bool isSW1Enabled() const { return SW1_enabled; }
-  inline bool isSW3Enabled() const { return SW3_enabled; }
+  inline bool isSwEnabled(unsigned int index) const { return SW_enabled[index]; }
 };
 
 void CSwitchHanlder::initSw()
@@ -59,45 +77,94 @@ void CSwitchHanlder::initSw()
   }
 }
 
+void CSwitchHanlder::initLed()
+{
+  for(uint8_t i=ARD_PIN_LED_1; i<=ARD_PIN_LED_3; ++i)
+  {
+    pinMode(i, OUTPUT);
+  }
+}
+
 void CSwitchHanlder::readSw()
 {
   //read from switches
-  SW1_current_state = digitalRead(ARD_PIN_SW_1);
-  SW3_current_state = digitalRead(ARD_PIN_SW_3);
+  for (uint8_t i=0, j=ARD_PIN_SW_1; i<NUM_OF_SW; ++i, j++)
+  {
+    SW_current_state[i] = digitalRead(j);
+  }
 }
 
 CSwitchHanlder::CSwitchHanlder() :
-  SW1_last_state{-1},
-  SW3_last_state{-1},
-  SW1_enabled{false},
-  SW3_enabled{false}
+  SW_last_state{-1},
+  SW_enabled{false}
 {
   CSwitchHanlder::initSw();
+  CSwitchHanlder::initLed();
+}
+
+void CSwitchHanlder::updateLed(e_actions action) const
+{
+  const bool* ignition_ptr = NULL;
+  switch(action)
+  {
+    case e_actions::UP:
+    {
+      ignition_ptr = led_up;
+      break;
+    }
+    case e_actions::DOWN:
+    {
+      ignition_ptr = led_down;
+      break;
+    }
+    case e_actions::RAND:
+    {
+      ignition_ptr = led_random;
+      break;
+    }
+    case e_actions::NONE:
+    {
+      ignition_ptr = led_none;
+      break;
+    }
+    default:
+    {
+      return;
+    }
+  }
+  for (uint8_t i=0, j=ARD_PIN_LED_1; j<=ARD_PIN_LED_3; i++, ++j)
+  {
+    digitalWrite(j, (ignition_ptr[i]) ? HIGH : LOW);
+  }
 }
 
 void CSwitchHanlder::checkSw()
 {
   CSwitchHanlder::readSw();
-  // reset booleans
-  SW1_enabled = SW3_enabled = false;
 
   // check is current state same as last
-  SW1_enabled = (SW1_current_state != SW1_last_state);
-  if (SW1_enabled)
+  for (uint8_t i=0; i<NUM_OF_SW; ++i)
   {
-    // if not avoid check for other SW
-    goto assign;
+    if (SW_current_state[i] != SW_last_state[i])
+    {
+      memset(SW_enabled, false, sizeof(SW_enabled));
+      SW_enabled[i] = true;
+      memcpy(SW_last_state, SW_current_state, sizeof(SW_current_state));
+      break;
+    }
   }
-  // check is current state same as last
-  SW3_enabled = (SW3_current_state != SW3_last_state);
-assign:
-  SW1_last_state = SW1_current_state;
-  SW3_last_state = SW3_current_state;
 }
 
 bool CSwitchHanlder::isSwStateUpdated() const
 {
-  return (SW1_enabled | SW3_enabled);
+  for (uint8_t i=0; i<NUM_OF_SW; ++i)
+  {
+    if (SW_enabled[i])
+    {
+      return true; 
+    }
+  }
+  return false;
 }
 
 /* @brief class handles 7-Seg init, updates counter according to switch states and writes LOW/HIGH signals to 7-seg outputs */
@@ -106,6 +173,7 @@ class C7SegHanlder
 private:
   uint8_t counter;
   int8_t incrementStep;
+  e_actions action;
 
   void initDisplay();
   void updateDisplay(const bool* segArr);
@@ -127,7 +195,8 @@ void C7SegHanlder::initDisplay()
 
 C7SegHanlder::C7SegHanlder() :
   counter{0},
-  incrementStep{1}
+  incrementStep{1},
+  action{e_actions::NONE}
 {
   C7SegHanlder::initDisplay();
 }
@@ -144,15 +213,35 @@ void C7SegHanlder::updateCounter(const CSwitchHanlder& sw)
 { 
   if (sw.isSwStateUpdated())
   {
-    incrementStep = (sw.isSW1Enabled()) ? 1 : -1;
+    for (uint8_t i=0; i<NUM_OF_SW; ++i)
+    {
+      if (sw.isSwEnabled(i))
+      {
+        action = static_cast<e_actions>(i);
+      }
+    }
   }
-  if (!counter && -1 == incrementStep)
+  if (e_actions::NONE == action)
   {
-    // return to UPPER value
-    counter = segPtrSize;
+    goto update_led;
   }
-  counter = (counter+incrementStep) % segPtrSize;
+  else if (e_actions::RAND == action)
+  {
+    counter = random(0, segPtrSize);
+  }
+  else
+  {
+    incrementStep = (e_actions::UP == action) ? (+1) : (-1);
+    if (!counter && -1 == incrementStep)
+    {
+      // return to UPPER value
+      counter = segPtrSize;
+    }
+    counter = (counter+incrementStep) % segPtrSize;
+  }
   C7SegHanlder::updateDisplay(*segPtr[counter]);
+update_led:
+  sw.updateLed(action);
 }
 
 static C7SegHanlder _7seg;
